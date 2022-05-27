159bc131d2e9c020d55a177d2e6747e27aea46a9
syed-asif-mn <syedasifm.n@gmail.com>
2022-05-27 19:50:14 +0530

    1 file modified

--- src/app/View/view.component.ts
+++ src/app/View/view.component.ts
@@ -7,39 +7,41 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./view.component.css'],
 })
 export class ViewComponent {
-
   project: string = '';
-  
+
   serverFrameworks = ['C#', 'Python', 'Java', 'NodeJS', 'DJango'];
   clientFrameworks = ['Angular', 'React', 'JS'];
-  languages = ['English', 'हिंदी', 'Français', 'Español', '中文', '日本語']
-  
+  languages = ['English', 'हिंदी', 'Français', 'Español', '中文', '日本語'];
+
   isSubmitted = false;
 
-  constructor(public fb: FormBuilder, private translateService: TranslateService) {
+  constructor(
+    public fb: FormBuilder,
+    private translateService: TranslateService
+  ) {
     this.translateService.use('en');
-    }
-    
-    changeLanguage(index: number) {
-    let codes = ['en', 'hin', 'fr', 'es', 'zh', 'ja']
+  }
+
+  changeLanguage(index: number) {
+    let codes = ['en', 'hin', 'fr', 'es', 'zh', 'ja'];
     this.translateService.use(codes[index]);
-    }
+  }
 
   registrationForm = this.fb.group({
     serverName: ['', [Validators.required]],
     clientName: ['', [Validators.required]],
-    projectName:  ['', [Validators.required]]
+    projectName: ['', [Validators.required]],
   });
 
   changeServer(e: any) {
     this.serverName?.setValue(e.target.value, {
-      onlySelf: true
+      onlySelf: true,
     });
   }
 
   changeClient(e: any) {
     this.clientName?.setValue(e.target.value, {
-      onlySelf: true
+      onlySelf: true,
     });
   }
 
@@ -52,7 +54,7 @@ export class ViewComponent {
     return this.registrationForm.get('clientName');
   }
 
-  get projectName(){
+  get projectName() {
     return this.registrationForm.get('projectName');
   }
 
@@ -61,27 +63,47 @@ export class ViewComponent {
     if (!this.registrationForm.valid) {
       false;
     } else {
-      alert(JSON.stringify(this.registrationForm.value, null,1));
-      var data = JSON.stringify({
-        "serverFramework": 1,
-        "clientFramework": 2,
-        "projectName": "airbus_test1"
-        });
-        
-        var xhr = new XMLHttpRequest();
-        xhr.withCredentials = true;
-        
-        xhr.addEventListener("readystatechange", function() {
-        if(this.readyState === 4) {
-        console.log(this.responseText);
+      alert(JSON.stringify(this.registrationForm.value, null, 1));
+      // var data = JSON.stringify({
+      //   "serverFramework": 1,
+      //   "clientFramework": 2,
+      //   "projectName": "airbus_test1"
+      //   });
+
+      //   var xhr = new XMLHttpRequest();
+      //   xhr.withCredentials = true;
+
+      //   xhr.addEventListener("readystatechange", function() {
+      //   if(this.readyState === 4) {
+      //   console.log(this.responseText);
+      //   }
+      //   });
+
+      //   xhr.open("POST", "http://d3d3-2409-4055-1-34f9-442-90-f1fd-74a7.ngrok.io/api/v1/create-project");
+      //   xhr.setRequestHeader("Content-Type", "application/json");
+      //   xhr.setRequestHeader("Cookie", "csrftoken=Ekkns2E4gRIgZ7yBCJadRbdlMyLimPXU02U1HVE8hRU61q996yILmZvDSQJJbUr7");
+
+      //   xhr.send(data);
+
+      var xhr = new XMLHttpRequest();
+      xhr.withCredentials = true;
+
+      xhr.addEventListener('readystatechange', function () {
+        if (this.readyState === 4) {
+          console.log(this.responseText);
         }
-        });
-        
-        xhr.open("GET", "http://d3d3-2409-4055-1-34f9-442-90-f1fd-74a7.ngrok.io/api/v1/list");
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.setRequestHeader("Cookie", "csrftoken=Ekkns2E4gRIgZ7yBCJadRbdlMyLimPXU02U1HVE8hRU61q996yILmZvDSQJJbUr7");
-        
-        xhr.send(data);
+      });
+
+      xhr.open(
+        'GET',
+        'https://d3d3-2409-4055-1-34f9-442-90-f1fd-74a7.ngrok.io/api/v1/list'
+      );
+      xhr.setRequestHeader(
+        'Cookie',
+        'csrftoken=Ekkns2E4gRIgZ7yBCJadRbdlMyLimPXU02U1HVE8hRU61q996yILmZvDSQJJbUr7'
+      );
+
+      xhr.send();
     }
   }
 }
