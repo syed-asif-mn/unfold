14d962ec80f89ed0455951715d899baaa3f03320
syed-asif-mn <syedasifm.n@gmail.com>
2022-05-27 19:47:32 +0530

    1 file modified

--- src/app/View/view.component.ts
+++ src/app/View/view.component.ts
@@ -77,7 +77,7 @@ export class ViewComponent {
         }
         });
         
-        xhr.open("POST", "http://d3d3-2409-4055-1-34f9-442-90-f1fd-74a7.ngrok.io/api/v1/list");
+        xhr.open("GET", "http://d3d3-2409-4055-1-34f9-442-90-f1fd-74a7.ngrok.io/api/v1/list");
         xhr.setRequestHeader("Content-Type", "application/json");
         xhr.setRequestHeader("Cookie", "csrftoken=Ekkns2E4gRIgZ7yBCJadRbdlMyLimPXU02U1HVE8hRU61q996yILmZvDSQJJbUr7");
         
