4a8c923a73f28d8c085b3e56d6a32731a0b715de
syed-asif-mn <syedasifm.n@gmail.com>
2022-05-27 21:05:20 +0530

    2 files modified

--- src/app/Communication/communication.service.ts
+++ src/app/Communication/communication.service.ts
@@ -1,12 +1,58 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
+import * as FileSaver from 'file-saver';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CommunicationService {
+  getList() {
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
 
-  serverFrameworks = ['C#', 'Python', 'Java', 'NodeJS', 'DJango'];
-  clientFrameworks = ['Angular', 'React', 'JS'];
-  languages = ['English', 'हिंदी', 'Français', 'Español', '中文', '日本語'];
-  codes = ['en', 'hin', 'fr', 'es', 'zh', 'ja'];
+    xhr.addEventListener('readystatechange', function () {
+      if (this.readyState === 4) {
+        console.log(this.responseText);
+      }
+    });
+
+    xhr.open(
+      'GET',
+      'https://d3d3-2409-4055-1-34f9-442-90-f1fd-74a7.ngrok.io/api/v1/list'
+    );
+    xhr.setRequestHeader(
+      'Cookie',
+      'csrftoken=Ekkns2E4gRIgZ7yBCJadRbdlMyLimPXU02U1HVE8hRU61q996yILmZvDSQJJbUr7'
+    );
+
+    xhr.send();
+  }
+  downloadData() {
+    var data = JSON.stringify({
+      serverFramework: 1,
+      clientFramework: 2,
+      projectName: 'airbus_test1',
+    });
+
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener('readystatechange', function () {
+      if (this.readyState === 4) {
+        var blob = new Blob([data], { type: 'application/x-tar' });
+        FileSaver.saveAs(blob, 'report.tgz');
+      }
+    });
+
+    xhr.open(
+      'POST',
+      'https://d3d3-2409-4055-1-34f9-442-90-f1fd-74a7.ngrok.io/api/v1/create-project'
+    );
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.setRequestHeader(
+      'Cookie',
+      'csrftoken=Ekkns2E4gRIgZ7yBCJadRbdlMyLimPXU02U1HVE8hRU61q996yILmZvDSQJJbUr7'
+    );
+
+    xhr.send(data);
+  }
 }
--- src/app/View/view.component.ts
+++ src/app/View/view.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { TranslateService } from '@ngx-translate/core';
-import * as FileSaver from 'file-saver';
 import { CommunicationService } from '../Communication/communication.service';
 import { ManagerService } from '../Manager/manager.service';
 
@@ -64,49 +63,7 @@ export class ViewComponent {
     if (!this.registrationForm.valid) {
       false;
     } else {
-      alert(JSON.stringify(this.registrationForm.value, null, 1));
-      var data = JSON.stringify({
-        "serverFramework": 1,
-        "clientFramework": 2,
-        "projectName": "airbus_test1"
-        });
-
-        var xhr = new XMLHttpRequest();
-        xhr.withCredentials = true;
-
-        xhr.addEventListener("readystatechange", function() {
-        if(this.readyState === 4) {
-          var blob = new Blob([data], {type: 'application/x-tar'});
-        FileSaver.saveAs(blob, "report.tgz");
-        console.log(this.responseText);
-        }
-        });
-
-        xhr.open("POST", "https://d3d3-2409-4055-1-34f9-442-90-f1fd-74a7.ngrok.io/api/v1/create-project");
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.setRequestHeader("Cookie", "csrftoken=Ekkns2E4gRIgZ7yBCJadRbdlMyLimPXU02U1HVE8hRU61q996yILmZvDSQJJbUr7");
-
-        xhr.send(data);
-
-    //   var xhr = new XMLHttpRequest();
-    //   xhr.withCredentials = true;
-
-    //   xhr.addEventListener('readystatechange', function () {
-    //     if (this.readyState === 4) {
-    //       console.log(this.responseText);
-    //     }
-    //   });
-
-    //   xhr.open(
-    //     'GET',
-    //     'https://d3d3-2409-4055-1-34f9-442-90-f1fd-74a7.ngrok.io/api/v1/list'
-    //   );
-    //   xhr.setRequestHeader(
-    //     'Cookie',
-    //     'csrftoken=Ekkns2E4gRIgZ7yBCJadRbdlMyLimPXU02U1HVE8hRU61q996yILmZvDSQJJbUr7'
-    //   );
-
-    //   xhr.send();
+      alert(`Download files for project: ${this.projectName}?`);
   }
 }
 }
