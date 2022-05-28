34bdea08eb1b6c580816cf1a1c3f02a2d5fbc88c
syed-asif-mn <syedasifm.n@gmail.com>
2022-05-28 11:15:00 +0530

    1 file modified

--- src/app/Communication/communication.service.ts
+++ src/app/Communication/communication.service.ts
@@ -28,26 +28,26 @@ export class CommunicationService {
   }
   
   downloadData(data: string) {
-    var xhr = new XMLHttpRequest();
-    xhr.withCredentials = true;
-
-    xhr.addEventListener('readystatechange', function () {
-      if (this.readyState === 4) {
-        var blob = new Blob([this.responseText]);
-        FileSaver.saveAs(blob, 'code.zip');
-      }
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("Cookie", "csrftoken=Ekkns2E4gRIgZ7yBCJadRbdlMyLimPXU02U1HVE8hRU61q996yILmZvDSQJJbUr7");
+    
+    var raw = JSON.stringify({
+      "serverFramework": 1,
+      "clientFramework": 2,
+      "projectName": "AirBus"
     });
-
-    xhr.open(
-      'POST',
-      'https://d3d3-2409-4055-1-34f9-442-90-f1fd-74a7.ngrok.io/api/v1/create-project'
-    );
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.setRequestHeader(
-      'Cookie',
-      'csrftoken=Ekkns2E4gRIgZ7yBCJadRbdlMyLimPXU02U1HVE8hRU61q996yILmZvDSQJJbUr7'
-    );
-
-    xhr.send(data);
+    
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
+    
+    fetch("https://8924-2409-4055-69d-b5f-f4ae-8473-9a06-b555.ngrok.io/api/v1/create-project", requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
   }
 }
